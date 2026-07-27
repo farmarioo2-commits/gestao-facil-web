00bc5e25eff0a7349bbe8de7a0b2fa9e79f0dfa6
Alexandre <farmarioo2@gmail.com>
2026-07-27 16:48:45 -0300

    ERP 2.0 - Atualização do servidor web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1971214145" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3492176384" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
