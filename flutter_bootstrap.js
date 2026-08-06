b1b31239797512884a5408222fc6fbadd6eb9fd9
Alexandre <farmarioo2@gmail.com>
2026-08-06 20:35:52 -0300

    Atualizar interface web do Gestao Facil

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1142135498" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1978853690" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
